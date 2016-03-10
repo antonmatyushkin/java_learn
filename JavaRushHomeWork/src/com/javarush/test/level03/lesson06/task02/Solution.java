4167312d41152a6c556c4fec6f8c23dd1e4ba35d
antonmatyushkin <anton.matyushkin@gmail.com>
2016-03-10 22:22:39 +0300

    12345

---
+++ JavaRushHomeWork/src/com/javarush/test/level03/lesson06/task02/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level03.lesson06.task02;
+
+/* Таблица умножения
+Выведи на экран таблицу умножения 10 на 10 в следующем виде:
+1 2 3 …
+2 4 6 …
+3 6 9 …
+…
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10;
+        System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " + f + " " + g + " " + h + " " + i + " " + j);
+        System.out.println(a * 2 + " " + b * 2 + " " + c * 2 + " " + d * 2 + " " + e * 2 + " " + f * 2 + " " + g * 2 + " " + h * 2 + " " + i * 2 + " " + j * 2);
+        System.out.println(a * 3 + " " + b * 3 + " " + c * 3 + " " + d * 3 + " " + e * 3 + " " + f * 3 + " " + g * 3 + " " + h * 3 + " " + i * 3 + " " + j * 3);
+        System.out.println(a * 4 + " " + b * 4 + " " + c * 4 + " " + d * 4 + " " + e * 4 + " " + f * 4 + " " + g * 4 + " " + h * 4 + " " + i * 4 + " " + j * 4);
+        System.out.println(a * 5 + " " + b * 5 + " " + c * 5 + " " + d * 5 + " " + e * 5 + " " + f * 5 + " " + g * 5 + " " + h * 5 + " " + i * 5 + " " + j * 5);
+        System.out.println(a * 6 + " " + b * 6 + " " + c * 6 + " " + d * 6 + " " + e * 6 + " " + f * 6 + " " + g * 6 + " " + h * 6 + " " + i * 6 + " " + j * 6);
+        System.out.println(a * 7 + " " + b * 7 + " " + c * 7 + " " + d * 7 + " " + e * 7 + " " + f * 7 + " " + g * 7 + " " + h * 7 + " " + i * 7 + " " + j * 7);
+        System.out.println(a * 8 + " " + b * 8 + " " + c * 8 + " " + d * 8 + " " + e * 8 + " " + f * 8 + " " + g * 8 + " " + h * 8 + " " + i * 8 + " " + j * 8);
+        System.out.println(a * 9 + " " + b * 9 + " " + c * 9 + " " + d * 9 + " " + e * 9 + " " + f * 9 + " " + g * 9 + " " + h * 9 + " " + i * 9 + " " + j * 9);
+        System.out.println(a * 10 + " " + b * 10 + " " + c * 10 + " " + d * 10 + " " + e * 10 + " " + f * 10 + " " + g * 10 + " " + h * 10 + " " + i * 10 + " " + j * 10);
+    }
+}
