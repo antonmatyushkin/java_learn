75b7eee32bdd83927a5503ba374c17fbd452ce91
antonmatyushkin <anton.matyushkin@gmail.com>
2016-03-16 18:55:46 +0300

    new solution tasks

--- JavaRushHomeWork/src/com/javarush/test/level04/lesson13/task04/Solution.java
+++ JavaRushHomeWork/src/com/javarush/test/level04/lesson13/task04/Solution.java
@@ -12,7 +12,17 @@ public class Solution
 {
     public static void main(String[] args) throws Exception
     {
-        //напишите тут ваш код
+        for (int i = 1; i <= 11; i++)
+        {
+            for (int j =1; j <= 10; j++)
+            {
+                if (i < j && i == 1)
+                {
+                    System.out.print("8");
+                }
+            }
+            System.out.println("8");
+        }
 
     }
 }
