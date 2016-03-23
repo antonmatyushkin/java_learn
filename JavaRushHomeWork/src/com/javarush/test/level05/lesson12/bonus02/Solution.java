1a30f3f881e86e6c580020243e85877a3451ed17
antonmatyushkin <anton.matyushkin@gmail.com>
2016-03-23 16:07:50 +0300

    ewr

--- JavaRushHomeWork/src/com/javarush/test/level05/lesson12/bonus02/Solution.java
+++ JavaRushHomeWork/src/com/javarush/test/level05/lesson12/bonus02/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.test.level05.lesson12.bonus02;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /* Нужно добавить в программу новую функциональность
 Задача: Программа вводит два числа с клавиатуры и выводит минимальное из них на экран.
@@ -13,22 +14,41 @@ public class Solution
 
     public static void main(String[] args) throws Exception
     {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] a = new int[7];
+        int i = 0;
+
+        Scanner in = new Scanner(System.in);
+        //BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        /*
         int a = Integer.parseInt(reader.readLine());
         int b = Integer.parseInt(reader.readLine());
-        int c = Integer.parseInt(reader.readLine());
-        int d = Integer.parseInt(reader.readLine());
-        int e = Integer.parseInt(reader.readLine());
 
-        int minimum;
+        int minimum = min(a, b);
 
         System.out.println("Minimum = " + minimum);
+        */
+
+        for (int element : a)
+        {
+
+            System.out.print("Введите " + (i + 1) + "-й элемент массива: ");
+            a[i] = in.nextInt();
+
+            System.out.println(a[i] + " - ");
+
+            i++;
+        }
+
+        for (int element : a)
+
+            System.out.print(element + " ");
+
     }
 
-
+    /*
     public static int min(int a, int b)
     {
         return a < b ? a : b;
     }
-
+    */
 }
