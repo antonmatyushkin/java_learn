d574e2a80b441afbf5ff63033826a9080fc62b44
a.matyushkin <a.matyushkin@securitycode.ru>
2016-11-25 18:29:00 +0300

    new task from lesson 07-12

--- JavaRushHomeWork/src/com/javarush/test/level07/lesson12/home06/Solution.java
+++ JavaRushHomeWork/src/com/javarush/test/level07/lesson12/home06/Solution.java
@@ -1,7 +1,8 @@
 package com.javarush.test.level07.lesson12.home06;
 
 /* Семья
-Создай класс Human с полями имя(String), пол(boolean),возраст(int), отец(Human), мать(Human). Создай объекты и заполни их так, чтобы получилось: Два дедушки, две бабушки, отец, мать, трое детей. Вывести объекты на экран.
+Создай класс Human с полями имя(String), пол(boolean),возраст(int), отец(Human), мать(Human).
+Создай объекты и заполни их так, чтобы получилось: Два дедушки, две бабушки, отец, мать, трое детей. Вывести объекты на экран.
 Примечание:
 Если написать свой метод String toString() в классе Human, то именно он будет использоваться при выводе объекта на экран.
 Пример вывода:
@@ -16,11 +17,43 @@ public class Solution
     public static void main(String[] args)
     {
         //напишите тут ваш код
+        Human grandfather1 = new Human("Борис", true, 95, null, null);
+        Human grandfather2 = new Human("Антон", true, 93, null, null);
+        Human grandmother1 = new Human("Надежда", false, 90, null, null);
+        Human grandmother2 = new Human("Валентина", false, 89, null, null);
+        Human father = new Human("Виктор", true, 70, grandfather2, grandmother2);
+        Human mother = new Human("Татьяна", false, 61, grandfather1, grandmother1);
+        Human childer1 = new Human("Антон", true, 31, father, mother);
+        Human childer2 = new Human("Михаил", true, 21, father, mother);
+        Human childer3 = new Human("Мария", false, 27, father, mother);
+
+        System.out.println(grandfather1);
+        System.out.println(grandfather2);
+        System.out.println(grandmother1);
+        System.out.println(grandmother2);
+        System.out.println(father);
+        System.out.println(mother);
+        System.out.println(childer1);
+        System.out.println(childer2);
+        System.out.println(childer3);
     }
 
     public static class Human
     {
         //напишите тут ваш код
+        private String name;
+        private boolean sex;
+        private int age;
+        private Human father;
+        private Human mother;
+
+        Human (String name, boolean sex, int age, Human father, Human mother) {
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+            this.father = father;
+            this.mother = mother;
+        }
 
         public String toString()
         {
